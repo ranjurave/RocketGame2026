202536cdd49f47a6ee12e7f57e0db1ce5faddab9
Ranju Raveendran <39932642+ranjurave@users.noreply.github.com>
2026-03-09 16:17:16 +1300

    Initial commit

---
+++ Assets/Script/RocketMovement.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class RocketMovement : MonoBehaviour
+{
+    [SerializeField]InputActionAsset inputActions;
+    InputAction rocketMove;
+    InputAction rotateRocket;
+    Rigidbody rocketRB;
+    [SerializeField] float thrustPower = 1000.0f;
+    [SerializeField] float rotationPower = 50.0f;
+ 
+
+    private void Awake() {
+        rocketMove = InputSystem.actions.FindAction("Thruster");
+        rotateRocket = InputSystem.actions.FindAction("RocketRotate");
+        rocketRB = GetComponent<Rigidbody>();
+    }
+
+    private void OnEnable() {
+        inputActions.FindActionMap("Player").Enable();
+    }
+
+    private void OnDisable() {
+        inputActions.FindActionMap("Player").Disable();
+    }
+
+    private void Update() {
+        if (rocketMove.IsPressed()) {
+            rocketRB.AddRelativeForce(Vector3.up * thrustPower * Time.deltaTime);
+        }
+
+        float Rotation = rotateRocket.ReadValue<float>();
+        transform.Rotate(0.0f, 0.0f, Rotation * Time.deltaTime * rotationPower);
+
+    }
+}
